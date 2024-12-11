528ed7a309eb767d135b7323f7eb975c314057d4
ken85illo <pabillokenneth81@gmail.com>
2024-12-11 19:38:50 +0800

    Add JLabel and Set Icon

--- src/Window.java
+++ src/Window.java
@@ -1,4 +1,5 @@
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class Window extends JFrame{
     private final int WIDTH = 500;
@@ -6,9 +7,17 @@ public class Window extends JFrame{
     private final String WINDOW_TITLE = "Stopwatch";
 
     Window() {
+        ImageIcon icon = new ImageIcon("stopwatch.jpg");
+        JLabel label = new JLabel();
+        label.setBackground(new Color(0xadacac));
+        label.setOpaque(true);
+
+        this.add(label);
         this.setTitle(WINDOW_TITLE);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setIconImage(icon.getImage());
         this.setSize(WIDTH, HEIGHT);
+        this.setResizable(false);
         this.setLocationRelativeTo(null);
         this.setVisible(true);
     }
